38f0995429c5d4b11b80677b7e75e1830c46dbad
zhangfei <zhangfei_eason@163.com>
2019-02-28 17:12:42 +0800

    style: upgrade theme next

--- themes/next/source/js/src/utils.js
+++ themes/next/source/js/src/utils.js
@@ -30,6 +30,7 @@ NexT.utils = NexT.$u = {
       });
 
     $('.fancybox').fancybox({
+      loop: true,
       helpers: {
         overlay: {
           locked: false
@@ -265,7 +266,7 @@ NexT.utils = NexT.$u = {
   },
 
   getContentVisibilityHeight: function() {
-    var docHeight = $('#content').height();
+    var docHeight = $('.container').height();
     var winHeight = $(window).height();
     var contentVisibilityHeight = docHeight > winHeight ? docHeight - winHeight : $(document).height() - winHeight;
     return contentVisibilityHeight;
@@ -292,7 +293,7 @@ NexT.utils = NexT.$u = {
 $(document).ready(function() {
 
   function wrapTable() {
-    $('table').wrap('<div class="table-container"></div>');
+    $('table').not('figure table').wrap('<div class="table-container"></div>');
   }
 
   /**
